6a63ac11ba961332d923814cc375237a77bf811f
udune <78453968+udune@users.noreply.github.com>
2025-04-16 18:01:28 +0900

    Update StateControl.cs

--- StateControl.cs
+++ StateControl.cs
@@ -1,28 +1,61 @@
-﻿using Constants;
+using Constants;
+using System;
 using System.Collections.Generic;
 
 public class StateControl
 {
-    public IState _currentState;
-    public AppState _currentStateEnum;
-    public ConventionExitState _conventionExitStateEnum = ConventionExitState.None;
-    Dictionary<AppState, IState> stateDic = new Dictionary<AppState, IState>();
+    private readonly Dictionary<AppState, IState> _stateDictionary = new();
+    private IState _currentState;
+    private AppState _currentStateEnum;
 
-    public void AddState(AppState _eAppState, IState _state)
+    public ConventionExitState CurrentConventionExitState { get; private set; } = ConventionExitState.None;
+    public AppState CurrentAppState => _currentStateEnum;
+
+    /// <summary>
+    /// 상태를 등록합니다. 중복된 상태는 예외 발생.
+    /// </summary>
+    public void AddState(AppState appState, IState state)
     {
-        stateDic.Add(_eAppState, _state);
+        if (_stateDictionary.ContainsKey(appState))
+        {
+            throw new ArgumentException($"State {appState} is already registered.");
+        }
+
+        _stateDictionary.Add(appState, state);
     }
 
-    public void SetState(AppState _state)
+    /// <summary>
+    /// 상태를 일괄 등록합니다.
+    /// </summary>
+    public void AddStates(params (AppState state, IState handler)[] states)
     {
+        foreach (var (state, handler) in states)
+        {
+            AddState(state, handler);
+        }
+    }
+
+    /// <summary>
+    /// 상태를 변경합니다.
+    /// </summary>
+    public void SetState(AppState newState)
+    {
+        if (!_stateDictionary.TryGetValue(newState, out var nextState))
+        {
+            throw new KeyNotFoundException($"State {newState} has not been registered.");
+        }
+
         _currentState?.OnExit();
-        _currentState = stateDic[_state];
-        _currentStateEnum = _state;
+        _currentState = nextState;
+        _currentStateEnum = newState;
         _currentState.OnEnter();
     }
 
-    public void SetConventionExitState(ConventionExitState _state)
+    /// <summary>
+    /// 컨벤션 종료 상태를 설정합니다.
+    /// </summary>
+    public void SetConventionExitState(ConventionExitState exitState)
     {
-        _conventionExitStateEnum = _state;
+        CurrentConventionExitState = exitState;
     }
 }
